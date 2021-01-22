29545cb5804ccfdb5b4ad9e55999ce75529ca460
Tamara Volic <tamara.volic@hotmail.com>
2021-01-22 09:28:19 +0100

    Signed-off-by: Tamara Volic <tamara.volic@hotmail.com>

---
+++ Aufgabe 2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Controller ctrl = new Controller();
+            try
+            {
+                ctrl.SetWidth(Convert.ToDouble(textBox1.Text));
+                ctrl.SetLength(Convert.ToDouble(textBox2.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            textBox3.Text = Convert.ToString(ctrl.CalculateResult());
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
---
+++ Aufgabe 4/Controller.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A04
+{
+    class Controller
+    {
+        String unit;
+        DateTime today = DateTime.Today;
+        DateTime birth;
+
+        public String Unit
+        {
+            set { unit = value; }
+            get { return unit; }
+        }
+        public DateTime Birth
+        {
+            set { birth = value; }
+            get { return birth; }
+        }
+        public DateTime Today
+        {
+            get { return today; }
+        }
+
+        public int GetTimeSpan()
+        {
+            int result = 0;
+
+            if (unit == null)
+            {
+                throw new Exception("Keine Einheit ausgewählt!");
+            }
+            TimeSpan span = today - birth;
+            if (unit == "years")
+            {
+                result = Convert.ToInt32(Math.Floor(span.TotalDays / 365));
+            }
+            else if (unit == "months")
+            {
+                result = Convert.ToInt32(Math.Floor(span.TotalDays / 30.4));
+            }
+            else if (unit == "weeks")
+            {
+                result = Convert.ToInt32(Math.Floor(span.TotalDays / 7));
+            }
+            else if (unit == "days")
+            {
+                result = Convert.ToInt32(span.TotalDays);
+            }
+            return result;
+
+        }
+    }
+}
